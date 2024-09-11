09af83b5fa54f4692cf222705594783c7e4c8f93
hci-Johann <johannhla7777@gmail.com>
2024-09-12 00:41:57 +0700

    signup updated

--- login.php
+++ login.php
@@ -166,7 +166,7 @@
 <main class="form-signin w-100 m-auto">
   <form>
     
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Sign in to your account</h1>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
